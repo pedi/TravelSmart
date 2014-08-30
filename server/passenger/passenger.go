2a190750c56afb831d08d0bfd6de2ed4c2e1a455
Shipeng Xu <BillHsu.x@gmail.com>
2014-08-30 13:39:23 +0800

    progress in passenger data collection services

--- server/passenger/passenger.go
+++ server/passenger/passenger.go
@@ -17,9 +17,73 @@ type Passenger struct {
 	mutex         *sync.Mutex
 }
 
-func NewPassengers() *Passenger {
+func NewPassenger() *Passenger {
 	return &Passenger{
 		make([]*PassengerEntry, 0),
 		new(sync.Mutex),
 	}
+}
+
+func (g *Passenger) AddEntry(busID []int, originBusStopID int, destinationBusStopID int) int {
+	g.mutex.Lock()
+	defer g.mutex.Unlock()
+
+	newId := len(g.passengerPool)
+	newEntry := &PassengerEntry{
+		newId,
+		busID,
+		originBusStopID,
+		destinationBusStopID,
+	}
+
+	g.passengerPool = append(g.passengerPool, newEntry)
+	return newId
+}
+
+func (g *Passenger) RemoveEntry(id int) error {
+	g.mutex.Lock()
+	defer g.mutex.Unlock()
+
+	if id < 0 || id >= len(g.passengerPool) ||
+		g.passengerPool[id] == nil {
+		return fmt.Errorf("invalid id")
+	}
+
+	g.passengerPool[id] = nil
+	g.passengerPool = append(g.passengerPool[:id], g.passengerPool[id+1:]...)
+
+	return nil
+}
+
+func (g *Passenger) GetEntry(id int) (*PassengerEntry, error) {
+	if id < 0 || id >= len(g.passengerPool) ||
+		g.passengerPool[id] == nil {
+		return nil, fmt.Errorf("invalid id")
+	}
+
+	return g.passengerPool[id], nil
+}
+
+func (g *Passenger) GetAllEntries() []*PassengerEntry {
+	entries := make([]*PassengerEntry, 0)
+
+	// Iterate through all existig entries.
+	for _, entry := range g.passengerPool {
+		if entry != nil {
+			// Entry is not nil, so we want to return it.
+			entries = append(entries, entry)
+		}
+	}
+
+	return entries
+}
+
+// RemoveAllEntries removes all entries from the Guest Book.
+func (g *Passenger) RemoveAllEntries() {
+	// Acquire our lock and make sure it will be released.
+	g.mutex.Lock()
+	defer g.mutex.Unlock()
+
+	// Reset guestbook to a new empty one.
+	g.passengerPool = []*PassengerEntry{}
 }
