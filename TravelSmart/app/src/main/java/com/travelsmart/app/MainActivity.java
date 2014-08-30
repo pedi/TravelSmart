3f0f000b47f60716a3b209eb5da10a70a9b15ce1
Qiao Liang <qiaoliang89@gmail.com>
2014-08-30 15:01:39 +0800

    Added Search bar

--- TravelSmart/app/src/main/java/com/travelsmart/app/MainActivity.java
+++ TravelSmart/app/src/main/java/com/travelsmart/app/MainActivity.java
@@ -1,15 +1,86 @@
 package com.travelsmart.app;
 
 import android.app.Activity;
+import android.app.SearchManager;
+import android.app.SearchableInfo;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import java.util.Arrays;
+import java.util.List;
+
+
+public class MainActivity extends Activity implements SearchView.OnQueryTextListener {
+
+    private SearchView mSearchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.search_bar, menu);
+        this.menu = menu;
+        MenuItem searchItem = menu.findItem(R.id.search);
+        mSearchView = (SearchView) searchItem.getActionView();
+        setupSearchView(searchItem);
+
+        return true;
+
+    }
+
+
+    private void setupSearchView(MenuItem searchItem) {
+
+        if (isAlwaysExpanded()) {
+            mSearchView.setIconifiedByDefault(false);
+        } else {
+            searchItem.setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_IF_ROOM
+                    | MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW);
+        }
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        if (searchManager != null) {
+            List<SearchableInfo> searchables = searchManager.getSearchablesInGlobalSearch();
+
+            SearchableInfo info = searchManager.getSearchableInfo(getComponentName());
+            for (SearchableInfo inf : searchables) {
+                if (inf.getSuggestAuthority() != null
+                        && inf.getSuggestAuthority().startsWith("applications")) {
+                    info = inf;
+                }
+            }
+            mSearchView.setSearchableInfo(info);
+        }
+
+        mSearchView.setOnQueryTextListener(this);
+    }
+
+    @Override
+    public boolean onQueryTextChange(String query) {
+        Toast.makeText(this, "Inputing = " + query, Toast.LENGTH_SHORT );
+        return false;
+
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        Toast.makeText(this, "Submitted = " + query, Toast.LENGTH_SHORT );
+        return false;
+    }
+    
+    protected boolean isAlwaysExpanded() {
+        return false;
     }
 }
